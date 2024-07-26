69ad98826291ea570e0ee137f7a99a7b085fe4d2
GitHub Action <137705289+trbernstein@users.noreply.github.com>
2024-07-26 02:02:25 +0000

    create-project-plist 1.1.1

--- Formula/create-project-plist.rb
+++ Formula/create-project-plist.rb
@@ -1,8 +1,8 @@
 class CreateProjectPlist < Formula
   desc "Command-line tool to generate a Info.plist for Swift package projects"
   homepage "https://github.com/astzweig/swift-project-info-plist"
-  url "https://github.com/astzweig/swift-project-info-plist/releases/download/1.0.0/create-project-plist.v1.0.0.tar.gz"
-  sha256 "b7189a31bc996c1677e316a3f4cb3f45fe81b8605df11daf301f2eb54e30b5b3"
+  url "https://github.com/astzweig/swift-project-info-plist/releases/download/1.1.1/create-project-plist.v1.1.1.tar.gz"
+  sha256 "edffd54df0ec37a7304a8a07a5618f4d2c0a7e0ecc2506b79bd2adb9e40617e4"
   license "EUPL-1.2"
   depends_on :macos
 
